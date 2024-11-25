510b2619b3f76abf20439f6f38d4f960bee9aab9
Joan Puigcerver <joapuiib@gmail.com>
2024-11-25 10:57:17 +0100

    tests directory data source with slash ending

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -49,6 +49,38 @@ def test_folder_source():
             },
     }
 
+def test_folder_source_slash():
+    mkdocs_config = load_config(
+        "tests/mkdocs.yml",
+        plugins={
+            "data": {'sources': {'data': 'tests/data/'}}
+        },
+    )
+
+    build(mkdocs_config)
+
+    dataPlugin = mkdocs_config["plugins"]["data"]
+    data = dataPlugin.sources["data"]
+
+    assert data == {
+            "a": {
+                "a1": 1,
+                "a2": "text",
+            },
+            "dir1": {
+                "b": {
+                    "b1": 2,
+                    "b2": "text",
+                },
+            },
+            "dir2": {
+                "c": {
+                    "c1": 3,
+                    "c2": "text",
+                },
+            },
+    }
+
 def test_file_source():
     mkdocs_config = load_config(
         "tests/mkdocs.yml",
